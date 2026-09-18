e1324f087458602c8f7e1ba662bb529d1491fba1
yuxuan2603454 <2603454@sit.singaporetech.edu.sg>
2026-09-18 16:15:43 +0800

    Add tax calculation function

--- modular_auditor.py
+++ modular_auditor.py
@@ -14,3 +14,7 @@ def get_valid_input():
 def process_delivery(current_total, new_value):
     new_total = current_total + new_value
     return new_total
+
+def calculate_tax(amount):
+    tax = amount * 0.10
+    return tax
